948951ec7d04f712038816922ba7c8321f093204
Pavel <awesome1.cmo@gmail.com>
2026-07-16 00:26:04 +0200

    pridano fibbonaciho tlactiko

--- WebApplication1/WebApplication1/Default.aspx.cs
+++ WebApplication1/WebApplication1/Default.aspx.cs
@@ -1,17 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace WebApplication1
 {
-    public partial class _Default : Page
+    public partial class Default : System.Web.UI.Page
     {
+        private int AktualniCislo
+        {
+            get
+            {
+                if (ViewState["AktualniCislo"] == null)
+                {
+                    ViewState["AktualniCislo"] = 1;
+                }
+
+                return (int)ViewState["AktualniCislo"];
+            }
+            set
+            {
+                ViewState["AktualniCislo"] = value;
+            }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                AktualniCislo = 1;
+                lblCislo.Text = AktualniCislo.ToString();
+            }
+        }
 
+        protected void btnFibonacci_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtPocet.Text, out int pocet) ||
+                pocet < 1 ||
+                pocet > 90)
+            {
+                lblVysledek.Text =
+                    "Zadejte počet prvků v rozsahu 1 až 90.";
+                return;
+            }
+
+            List<long> posloupnost = new List<long>();
+
+            long prvni = 0;
+            long druhy = 1;
+
+            for (int i = 0; i < pocet; i++)
+            {
+                posloupnost.Add(prvni);
+
+                long dalsi = prvni + druhy;
+                prvni = druhy;
+                druhy = dalsi;
+            }
+
+            lblVysledek.Text = string.Join(", ", posloupnost);
+        }
+
+        protected void btnDalsiCislo_Click(object sender, EventArgs e)
+        {
+            AktualniCislo++;
+            lblCislo.Text = AktualniCislo.ToString();
         }
     }
 }
